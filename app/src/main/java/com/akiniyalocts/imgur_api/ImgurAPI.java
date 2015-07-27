cea272cb4afd714c323cfd1788a2b2f41e037aaa
Christopher Jakob <jakob.chrisli@gmail.com>
2015-07-27 21:53:50 +0200

    /album/{id} call added

--- app/src/main/java/com/akiniyalocts/imgur_api/ImgurAPI.java
+++ app/src/main/java/com/akiniyalocts/imgur_api/ImgurAPI.java
@@ -1,9 +1,17 @@
 package com.akiniyalocts.imgur_api;
 
+import com.akiniyalocts.imgur_api.model.Album;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
 /**
  * Created by anthony on 7/26/15.
  */
 public interface ImgurAPI {
 
+    @GET("/album/{id}")
+    void getAlbumInfo(@Path("id") int id, Callback<Album> cb);
 
 }
